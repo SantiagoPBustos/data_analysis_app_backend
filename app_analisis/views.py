c7cfd064b546399e6a3f145ed98c91b91ff2134f
Santiago <david.pina@uptc.edu.co>
2023-11-20 21:47:39 -0500

    Add post method

--- app_analisis/views.py
+++ app_analisis/views.py
@@ -48,7 +48,7 @@ class DataInstitution(APIView):
         response = JsonResponse(
             {
                 "status": 200,
-                "data": worstAveragesPerComponentByMunicipality(data, "2.", LOCATIVAS, 70),
+                "data": worstAveragesPerComponentByMunicipality(data, "2.", LOCATIVAS, 20),
             }
         )
 
