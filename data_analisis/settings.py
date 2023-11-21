204223251dd06636d6e1aad27ba5daf9afaffd53
Santiago <david.pina@uptc.edu.co>
2023-11-21 10:25:40 -0500

    Add post method

--- data_analisis/settings.py
+++ data_analisis/settings.py
@@ -74,14 +74,18 @@ CORS_ALLOW_METHODS = [
 ]
 
 CORS_ALLOWED_ORIGINS = ["https://secretaria-salud-app.vercel.app",
-                        "http://secretaria-salud-app.vercel.app",
+                        "http://secretaria-salud-app.vercel.app",    
+                        "https://secretaria-salud-app.vercel.app/"
+                        "http://secretaria-salud-app.vercel.app/",
                         "http://localhost:5173",
                         "https://localhost:5173"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://secretaria-salud-app.vercel.app",
     "http://secretaria-salud-app.vercel.app",
-    "http://localhost:5173", 
+    "https://secretaria-salud-app.vercel.app/"
+    "http://secretaria-salud-app.vercel.app/",
+    "http://localhost:5173",
     "https://localhost:5173"
 ]
 
